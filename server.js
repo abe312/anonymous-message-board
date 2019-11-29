ce286219b4e93b9cacc9f6924b691cf00a76c67c
abe312 <abhineet312@gmail.com>
2019-11-29 20:35:53 +0530

    heroku bug

--- server.js
+++ server.js
@@ -64,10 +64,11 @@ const path = require('path');
 // Server static assets if in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('client/build'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
+  // app.use(express.static('client/build'));
+  // app.get('*', (req, res) => {
+  //   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  // });
+  app.use('/', express.static(path.join(__dirname, '/client/build')));
 }
 
 //Start our server and tests!
